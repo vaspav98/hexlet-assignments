d86c738410f3e980dfc1e57d98162db4c7657718
@hexlet/cli <support@hexlet.io>
2023-01-16 15:47:43 +0300

    submit java-collections-ru/maps

---
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -0,0 +1,38 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// BEGIN
+class App {
+    public static final String SENTENCE = "the java is the best programming language java";
+
+    public static void main(String[] args) {
+        System.out.println(toString(getWordCount(SENTENCE)));
+
+    }
+
+    public static Map<String, Integer> getWordCount(String sentence) {
+        String[] arrayOfWords = sentence.split(" ");
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : arrayOfWords) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+        return map;
+    }
+
+    public static String toString(Map<String, Integer> map) {
+        var result = new StringBuilder();
+        result.append("{");
+        for (String key : map.keySet()) {
+            result.append("\n  ").append(key).append(": ").append(map.get(key));
+        }
+        result.append("\n}");
+        return result.toString();
+    }
+}
+//END
