7c46a3d68940c0305e11f5c0c039361e3a8838dc
@hexlet/cli <support@hexlet.io>
2023-06-01 17:54:18 +0300

    submit java-web-ru/session

---
+++ java-web-ru/session/src/main/java/exercise/servlet/SessionServlet.java
@@ -0,0 +1,90 @@
+package exercise.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.RequestDispatcher;
+
+import java.util.Map;
+
+import static exercise.App.getUsers;
+import exercise.Users;
+
+public class SessionServlet extends HttpServlet {
+
+    private Users users = getUsers();
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+                throws IOException, ServletException {
+
+        if (request.getRequestURI().equals("/login")) {
+            showLoginPage(request, response);
+            return;
+        }
+
+        response.sendError(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request,
+                      HttpServletResponse response)
+                throws IOException, ServletException {
+
+        switch (request.getRequestURI()) {
+            case "/login" -> login(request, response);
+            case "/logout" -> logout(request, response);
+            default -> response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    private void showLoginPage(HttpServletRequest request,
+                               HttpServletResponse response)
+                 throws IOException, ServletException {
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/login.jsp");
+        requestDispatcher.forward(request, response);
+    }
+
+    private void login(HttpServletRequest request,
+                               HttpServletResponse response)
+                 throws IOException, ServletException {
+
+        // BEGIN
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        Map<String, String> user = users.findByEmail(email);
+
+        HttpSession session = request.getSession();
+
+        if (user == null || !user.get("password").equals(password)) {
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/login.jsp");
+            session.setAttribute("flash", "Неверные логин или пароль");
+            request.setAttribute("user", user);
+            response.setStatus(422);
+            requestDispatcher.forward(request, response);
+            return;
+        }
+
+        session.setAttribute("userId", user.get("id"));
+        session.setAttribute("flash", "Вы успешно вошли");
+        response.sendRedirect("/");
+        // END
+    }
+
+    private void logout(HttpServletRequest request,
+                               HttpServletResponse response)
+                 throws IOException {
+
+        // BEGIN
+        HttpSession session = request.getSession();
+        session.removeAttribute("userId");
+        session.setAttribute("flash", "Вы успешно вышли");
+        response.sendRedirect("/");
+        // END
+    }
+}
---
+++ java-web-ru/session/src/main/webapp/new.jsp
@@ -0,0 +1,25 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="tag" tagdir="/WEB-INF/tags" %>
+
+<tag:application>
+    <h1>Регистрация</h1>
+    <form action="/users/new" method="post">
+        <div class="mb-3">
+            <label>Имя</label>
+            <input class="form-control" type="text" name="firstName" value='${user.getOrDefault("firstName", "")}'>
+        </div>
+        <div class="mb-3">
+            <label>Фамилия</label>
+            <input class="form-control" type="text" name="lastName" value='${user.getOrDefault("lastName", "")}'>
+        </div>
+        <div class="mb-3">
+            <label>Email</label>
+            <input class="form-control" type="email" name="email">
+        </div>
+        <div class="mb-3">
+            <label>Password</label>
+            <input class="form-control" type="password" name="password">
+        </div>
+        <button class="btn btn-primary" type="submit">Создать</button>
+    </form>
+</tag:application>
