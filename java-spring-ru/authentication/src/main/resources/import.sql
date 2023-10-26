526098a3701b7435b6719ef8947a86f541f675b9
@hexlet/cli <support@hexlet.io>
2023-10-26 16:09:16 +0300

    submit java-spring-ru/authentication

---
+++ java-spring-ru/authentication/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+-- Используйте эти данные для аутентификации
+-- username: Alexis password:
+
+
+-- В самой базе пароль храним в зашифрованном виде
+INSERT INTO USERS (ID, USERNAME, EMAIL, PASSWORD) VALUES (1, 'Alexis', 'al@gmail.com', '$2a$12$lP1HdpYa4yT./wnilPzf0OoxaVHJunEkkxw4SwkYL5obPjq.fG8Xy');
