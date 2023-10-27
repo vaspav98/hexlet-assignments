5a7f5f2e1360432635a0653385beeafef412fb3b
@hexlet/cli <support@hexlet.io>
2023-10-27 14:12:34 +0300

    submit java-spring-ru/queues

---
+++ java-spring-ru/queues/consumer/src/main/java/exercise/QueueListener.java
@@ -0,0 +1,35 @@
+package exercise;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+@Component
+public class QueueListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(QueueListener.class);
+
+    // Для наглядности в список будут добавляться все сообщения,
+    // которые получены из очереди
+    // Так их можно будет легко просмотреть
+    private List<String> messages = new ArrayList<>();
+
+    public List<String> getAllMessages() {
+        return messages;
+    }
+
+    // BEGIN
+    @RabbitListener(queues = "queue")
+    public void processQueue(String message) {
+        messages.add(message);
+        LOGGER.info("полученное сообщение: " + message);
+        LOGGER.info("список добавленных сообщений: " + message);
+    }
+
+    // END
+}
